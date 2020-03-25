62dd4ee44032371202fac5b296b8b0b2e0727b81
deep <hereisdanbrown@gmail.com>
2020-03-25 15:07:34 -0600

    Added a .env file attempting to hide api keys

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { createMarker, populateMarkers, getLocation } from "./scripts/google";
+
 // HTML Variables
 var h1 = $("h1");
 var container = $('.container');
@@ -228,77 +230,6 @@ function pageTurn(increment, page){
     return page;
 };
 
-// Google Maps Implementation
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(initMap, showError);
-    } else {
-        var x = document.getElementById("location");
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
-
-function showError(error) {
-    switch(error.code) {
-        case error.PERMISSION_DENIED:
-            x.innerHTML = "User denied the request for Geolocation."
-            break;
-        case error.POSITION_UNAVAILABLE:
-            x.innerHTML = "Location information is unavailable."
-            break;
-        case error.TIMEOUT:
-            x.innerHTML = "The request to get user location timed out."
-            break;
-        case error.UNKNOWN_ERROR:
-            x.innerHTML = "An unknown error occurred."
-            break;
-    }
-}
-
-function initMap(position) {
-    // The location of Denver
-    // var denver = {lat: 39.7392, lng: -104.9903};
-    // The map, centered on Denver
-    lat = position.coords.latitude;
-    lng = position.coords.longitude;
-    var map = new google.maps.Map(
-        document.getElementById('map'), {
-            zoom: 10, 
-            center: {lat: lat, lng: lng},
-        });
-    var marker = new google.maps.Marker({
-        position: {lat: lat, lng: lng},
-        map: map,
-    });
-    latLng = [lat, lng];
-  }
-
-function createMarker(event, color) {
-  var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(event._embedded.venues[0].location.latitude, event._embedded.venues[0].location.longitude),
-    title: event.name,
-    animation: google.maps.Animation.DROP
-  });
-//   marker.addListener()
-
-  marker.setIcon(`http://maps.google.com/mapfiles/ms/icons/${color}-dot.png`);
-  return marker;
-}
-
-function populateMarkers(storedEvents){
-    var map = new google.maps.Map(
-        document.getElementById('map'), {
-            zoom: 10, 
-            center: {lat: latLng[0], lng: latLng[1]},
-        });
-    for (event in storedEvents){
-        storedEvents[event].marker.setMap(map);    
-    };
-    var marker = new google.maps.Marker({
-        position: {lat: lat, lng: lng},
-        map: map,
-    });
-};
 
 // Initialization
 function momentConfig(){
@@ -313,7 +244,6 @@ function init(){
     getLocation();
     formOptionFiller("start");
     formOptionFiller("end");
-
 };
 
 init();
---
+++ scripts/google.js
@@ -0,0 +1,71 @@
+// Google Maps Implementation
+export function getLocation() {
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(initMap, showError);
+  } else {
+      var x = document.getElementById("location");
+      x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showError(error) {
+  switch(error.code) {
+      case error.PERMISSION_DENIED:
+          x.innerHTML = "User denied the request for Geolocation."
+          break;
+      case error.POSITION_UNAVAILABLE:
+          x.innerHTML = "Location information is unavailable."
+          break;
+      case error.TIMEOUT:
+          x.innerHTML = "The request to get user location timed out."
+          break;
+      case error.UNKNOWN_ERROR:
+          x.innerHTML = "An unknown error occurred."
+          break;
+  }
+}
+function initMap(position) {
+  // The location of Denver
+  // var denver = {lat: 39.7392, lng: -104.9903};
+  // The map, centered on Denver
+  lat = position.coords.latitude;
+  lng = position.coords.longitude;
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: { lat: lat, lng: lng }
+  });
+  var marker = new google.maps.Marker({
+    position: { lat: lat, lng: lng },
+    map: map
+  });
+  latLng = [lat, lng];
+}
+
+export function createMarker(event, color) {
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(
+      event._embedded.venues[0].location.latitude,
+      event._embedded.venues[0].location.longitude
+    ),
+    title: event.name,
+    animation: google.maps.Animation.DROP
+  });
+  //   marker.addListener()
+
+  marker.setIcon(`http://maps.google.com/mapfiles/ms/icons/${color}-dot.png`);
+  return marker;
+}
+
+export function populateMarkers(storedEvents) {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: { lat: latLng[0], lng: latLng[1] }
+  });
+  for (event in storedEvents) {
+    storedEvents[event].marker.setMap(map);
+  }
+  var marker = new google.maps.Marker({
+    position: { lat: lat, lng: lng },
+    map: map
+  });
+}
